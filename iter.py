5fc42bab81add040f3239524b90bfea01bb4088c
JucanaYu <705027616@qq.com>
2024-11-18 19:49:54 +0800

    commit

--- iter.py
+++ iter.py
@@ -1,7 +1,16 @@
 import sys
 
 list=[1,2,3,4]
-it=iter(list)   #迭代器化
+it=iter(list)   #迭代器初始化
+
+#使用for遍历
+for i in it:
+    print(i,end=" ")
+
+#在这里重新初始化了一个迭代器，是因为迭代器在输出之后就不会保存内容了
+list=[1,2,3,4]
+it=iter(list)   #迭代器初始化
+#使用next遍历
 while True:
     try:
         print(next(it),end=" ")
